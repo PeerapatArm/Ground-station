a8559e4de6e1adf0a54ffff261567c43f28164c7
PeerapatArm <96534987+PeerapatArm@users.noreply.github.com>
2022-01-02 10:26:25 +0700

    fix error

--- setting_file/_global.py
+++ setting_file/_global.py
@@ -1,22 +1,15 @@
-import PySide6
-from PySide6 import QtCore, QtGui, QtWidgets
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtCharts import *
-import PyQt5
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 from pyqtgraph import PlotWidget
-from datetime import datetime, date
+
 import time
-import os
 import sys
+import os
 import threading
-import serial
 import math
+import serial
 import folium
-import io
-import string
 import paho
+import string
 import geocoder
+from datetime import date, datetime
+
